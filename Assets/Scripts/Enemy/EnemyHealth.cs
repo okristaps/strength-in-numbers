d4a9106cc823bb0dacbd7efdc84d20d3c2ca14a8
okristaps <okristaps1@gmail.com>
2024-05-19 15:28:26 +0300

    spawn ammo

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,13 +6,24 @@ public class EnemyHealth : MonoBehaviour {
 
     public int maxHealth = 100;
     public int currentHealth;
+
+    [SerializeField]
+    private SpawnAmmo _spawnAmmo;
+
+
     void Start() {
         currentHealth = maxHealth;
+        _spawnAmmo = GetComponent<SpawnAmmo>();
     }
+
     public void TakeDamage(int amount) {
         currentHealth -= amount;
         if (currentHealth <= 0) {
+            Vector3 enemyPosition = transform.position;
             Destroy(gameObject);
+            _spawnAmmo.SpawnTrigger(enemyPosition);
         }
     }
+
+
 }
